f947e554ccd99ea317d53c9b19ba7753409de33b
krishna <krushnabhamare@gmail.com>
2018-12-26 14:21:54 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,57 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
+import { ReactiveFormsModule } from '@angular/forms';
+// Components
 import { AppComponent } from './app.component';
-
+import { MainComponent } from './main/main.component';
+import { VideosListComponent } from './main/videos-list/videos-list.component';
+import { VideosPlaylistComponent } from './main/videos-playlist/videos-playlist.component';
+import { VideosSearchComponent } from './main/videos-search/videos-search.component';
+import { VideoPlayerComponent } from './main/video-player/video-player.component';
+// Services
+import { YoutubeApiService } from './shared/services/youtube-api.service';
+import { YoutubePlayerService } from './shared/services/youtube-player.service';
+import { PlaylistStoreService } from './shared/services/playlist-store.service';
+import { NotificationService } from './shared/services/notification.service';
+import { BrowserNotificationService } from './shared/services/browser-notification.service';
+// Pipes
+import { VideoDurationPipe } from './shared/pipes/video-duration.pipe';
+import { VideoLikesViewsPipe } from './shared/pipes/video-likes-views.pipe';
+import { VideoNamePipe } from './shared/pipes/video-name.pipe';
+import { LazyScrollDirective } from './shared/directives/lazy-scroll/lazy-scroll.directive';
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    HttpModule,
+    ReactiveFormsModule
+  ],
   declarations: [
+    AppComponent,
+    MainComponent,
+
+    VideosListComponent,
+    VideosSearchComponent,
+    VideoPlayerComponent,
+    VideosPlaylistComponent,
+
+    VideoDurationPipe,
+    VideoLikesViewsPipe,
+    VideoNamePipe,
+
+    LazyScrollDirective
+  ],
+  bootstrap: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    YoutubeApiService,
+    YoutubePlayerService,
+    PlaylistStoreService,
+    NotificationService,
+    BrowserNotificationService
+  ]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,7 @@
-import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode } from '@angular/core';
+import { AppModule } from './app/';
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+enableProdMode();
 
-if (environment.production) {
-  enableProdMode();
-}
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+platformBrowserDynamic().bootstrapModule(AppModule);
